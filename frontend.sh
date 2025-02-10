7595893e14d31ba5e21957c78d747b752943aea4
imrankhan760 <devpractice470@gmail.com>
2025-02-09 21:25:44 -0600

    dfdf

--- frontend.sh
+++ frontend.sh
@@ -3,22 +3,21 @@ yum install nginx -y &>>/tmp/roboshop.log
 
 echo -e "\e[32m removing old content\e[0m"
 
-rm -rf /usr/share/nginx/html/*  >/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 
 echo -e "\e[34m downloading froent content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshop.log
-
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
 
 
 echo -e "\e[35m extract frontend content\e[0m" 
-cd /usr/share/nginx/html >/tmp/roboshop.log
-unzip /tmp/frontend.zip >/tmp/roboshop.log
+cd /usr/share/nginx/html &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
 #we need copy cofig file
 
 echo -e "\e[36m start frntend contend \e[0m"
 
-systemctl enable nginx  >/tmp/roboshop.log
+systemctl enable nginx  &>>/tmp/roboshop.log
 
-systemctl restart nginx >/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
