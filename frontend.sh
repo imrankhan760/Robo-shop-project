087e114e666e6e0ceb55613d2bd33472ddb365e0
imrankhan760 <devpractice470@gmail.com>
2025-02-09 21:05:25 -0600

    record logs

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,14 @@
 echo -e "\e[33minstall nginx server\e[0m"
-yum install nginx -y 
+yum install nginx -y >/tmp/roboshop.log
 
 echo -e "\e[32m removing old content\e[0m"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/*  >/tmp/roboshop.log
+
 
 echo -e "\e[34m downloading froent content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshop.log
+
 
 
 echo -e "\e[35m extract frontend content\e[0m" 
@@ -17,5 +19,6 @@ unzip /tmp/frontend.zip
 
 echo -e "\e[36m start frntend contend \e[0m"
 
-systemctl enable nginx  
-systemctl restart nginx 
+systemctl enable nginx  >/tmp/roboshop.log
+
+systemctl restart nginx >/tmp/roboshop.log
